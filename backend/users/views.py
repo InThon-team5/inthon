875dbefc68eeb6be93478647dc7f2a26396796e8
sw_vi.xi <sunwoo0251@gmail.com>
2025-11-15 21:42:59 +0900

    feat: 로그인 관련 view 추가

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
-from .serializers import UserCreateSerializer
-from .models import User # User 모델도 임포트 (Serializer가 사용하긴 하지만 명시적)
+from .serializers import UserCreateSerializer, MyTokenObtainPairSerializer
+from .models import User
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 class UserCreateView(generics.CreateAPIView):
     """
@@ -11,3 +12,10 @@ class UserCreateView(generics.CreateAPIView):
     serializer_class = UserCreateSerializer
     # 이 View가 다룰 모델을 지정 (필수는 아니나, 명시적으로)
     queryset = User.objects.all()
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    """
+    로그인 (POST /api/users/login/)
+    (JWT 토큰 발급)
+    """
+    serializer_class = MyTokenObtainPairSerializer
