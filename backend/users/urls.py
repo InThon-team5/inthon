cf6d3c28ac51958567b8bb527178b2135f502e5e
sw_vi.xi <sunwoo0251@gmail.com>
2025-11-15 21:55:27 +0900

    feat: 로그인 API 경로 추가

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserCreateView
+from .views import UserCreateView, MyTokenObtainPairView
 
 urlpatterns = [
     # POST /api/users/signup/ 경로로 요청이 오면 UserCreateView를 실행
     path('signup/', UserCreateView.as_view(), name='user-signup'),
+    # POST /api/users/login/ (JWT 토큰 발급)
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
